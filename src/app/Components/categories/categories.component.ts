1cdcceeb5c91d7fb7ece3e60ebb584cd46a674b9
Naor Talmor <naortalmornew@gmail.com>
2018-05-23 22:27:40 +0300

    Add Sweetalert and input validation for new category and delete

--- src/app/Components/categories/categories.component.ts
+++ src/app/Components/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../../Services/categories.service';
 import { Category } from '../../Objects/Category';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-categories',
@@ -12,9 +13,9 @@ export class CategoriesComponent implements OnInit {
   public error: string;
   public newCategory: string;
   filter: string;
+  errorCatInput: Boolean = false;
 
-  constructor(private _categoriesService: CategoriesService)
-  {
+  constructor(private _categoriesService: CategoriesService) {
     // Get all categories
     this._categoriesService.getAllCategories().subscribe(allCategories => {
           this.categories = allCategories;
@@ -26,35 +27,77 @@ export class CategoriesComponent implements OnInit {
 
   public deleteCategory(category: Category) {
     this._categoriesService.removeCategory(category._id)
-          .subscribe(() => {
-            this.categories.splice(this.categories.indexOf(category), 1);
-          }, err => {
-            this.error = 'error deleting category';
-          });
+      .subscribe(() => {
+        this.categories.splice(this.categories.indexOf(category), 1);
+      }, err => {
+        this.error = 'error deleting category';
+      });
+
+    swal({
+      title: 'Processing...',
+      text: 'Deleting The Category - ' + category.name,
+      timer: 1500,
+      onOpen: () => {
+        swal.showLoading();
+      }
+    }).then(() => {
+      swal(
+        'Done, ',
+        'The Category ' + category.name + ' Deleted Successfuly',
+        'success'
+      );
+    });
   }
 
   public addCategory() {
-
+    const newCat = this.newCategory;
     // Check if not valid
-    if (!this.newCategory || this.newCategory.length > 50){
-        alert('name not valid');
+    if (!this.newCategory || this.newCategory.length > 50) {
+      this.errorCatInput = true;
+        swal({
+          type: 'error',
+          title: 'Bad Input!',
+          text: 'Please Insert New Category Name'
+        });
         return;
     }
     // Check if exist
-    if (this.categories.findIndex(c => c.name === this.newCategory) !== -1){
-        alert('name already exists!');
-        return;
+    if (this.categories.findIndex(c => c.name === this.newCategory) !== -1) {
+      this.errorCatInput = true;
+      swal({
+        type: 'error',
+        title: 'Bad Input!',
+        text: 'Category Name Already Exists'
+      });
+      return;
     }
 
     // Add
+    this.errorCatInput = false;
     this._categoriesService.addCategory(this.newCategory)
           .subscribe(res => {
             this.categories.push(res.json());
-            this.newCategory = "";
+            this.newCategory = '';
             //app.server.emit('categories', self.categories);
           }, err => {
             this.error = 'error adding category';
           });
+
+    swal({
+      title: 'Processing...',
+      text: 'Adding Category - ' + this.newCategory,
+      timer: 1500,
+      onOpen: () => {
+        swal.showLoading();
+      }
+    }).then(() => {
+      swal(
+        'Done, ',
+        'The Category ' + newCat + ' Added Successfuly',
+        'success'
+      );
+    });
+
   }
 
   ngOnInit() {
